f887c6ab3f4c6e3b46d12dd7be02b9452c4da7f4
Kevin Nguyen <johnnyblaze1999.sg@gmail.com>
2024-03-30 10:37:23 -0700

    Initial commit

---
+++ CompliantNode.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+/* CompliantNode refers to a node that follows the rules (not malicious)*/
+public class CompliantNode implements Node {
+
+    private boolean[] followees;
+    private Set<Transaction> pendingTransactions;
+
+    public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
+        // IMPLEMENT THIS
+        this.followees = new boolean[numRounds];
+        this.pendingTransactions = new HashSet<>();
+    }
+
+    public void setFollowees(boolean[] followees) {
+        // IMPLEMENT THIS
+        System.arraycopy(followees, 0, this.followees, 0, followees.length);
+    }
+
+    public void setPendingTransaction(Set<Transaction> pendingTransactions) {
+        // IMPLEMENT THIS
+        this.pendingTransactions = new HashSet<>(pendingTransactions);
+    }
+
+    public Set<Transaction> getProposals() {
+        // IMPLEMENT THIS
+        return this.pendingTransactions;
+    }
+
+    public void receiveCandidates(ArrayList<Integer[]> candidates) {
+        // IMPLEMENT THIS
+        for (Integer[] candidate : candidates){
+            int transactionId = candidate[0];
+            Transaction transaction = new Transaction(transactionId);
+            this.pendingTransactions.add(transaction);
+        }
+    }
+}
